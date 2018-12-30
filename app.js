c4bcca6c420ed2b91d85e041e6043511aa1a072f
Fadli Ramadhan <fadlir93@gmail.com>
2018-12-31 06:47:58 +0700

    Validation

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
-var flash = require('connect-flash')
+var flash = require('connect-flash');
+var validator = require('express-validator');
 
 var indexRouter = require('./routes/index');
 
@@ -23,6 +24,7 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(validator())
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(flash());
